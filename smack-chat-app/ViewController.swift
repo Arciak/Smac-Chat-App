2104732042f8e3be2d997fc894d5bcec3655ee75
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-20 11:25:33 +0100

    Initial commit

---
+++ smack-chat-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack-chat-app
+//
+//  Created by Artur Zarzecki on 20/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
