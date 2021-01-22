3bdffcf076f740fe5f282c111686639e4e5dbe34
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-22 09:43:33 +0100

    Creat LogIn VC and close btn, which allows gow abck to previois view

---
+++ smack-chat-app/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack-chat-app
+//
+//  Created by Artur Zarzecki on 22/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
---
+++ smack-chat-app/Utilities/Constans.swift
@@ -0,0 +1,14 @@
+//
+//  Constans.swift
+//  smack-chat-app
+//
+//  Created by Artur Zarzecki on 22/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import Foundation
+
+
+//Segues
+
+let TO_LOGIN = "toLogin"
