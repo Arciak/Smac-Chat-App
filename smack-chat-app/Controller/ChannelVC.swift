aedeb142ce683f96f47fbfe71d7860577bbf0fe8
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-28 07:22:49 +0100

    Bug during createing table view

--- smack-chat-app/Controller/ChannelVC.swift
+++ smack-chat-app/Controller/ChannelVC.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //Outlets
     // potrzebujemy outlet dla loginBtn bo zmieniamy nazwe w zaleznosci czy ktos jest zalogowany czy nie jest
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var tableView: UITableView!
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
         //listening
@@ -60,5 +63,26 @@ class ChannelVC: UIViewController {
             userImg.backgroundColor = UIColor.clear
         }
     }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    
     
 }
---
+++ smack-chat-app/View/ChannelCell.swift
@@ -0,0 +1,37 @@
+//
+//  ChannelCell.swift
+//  smack-chat-app
+//
+//  Created by Artur Zarzecki on 27/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state. What does the view look like for any of the rose table been selected
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2) as! CGColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell(channel: Channel) {
+        let title = channel.channelTitle ?? "" //if you cant find value return empty string
+        channelName.text = "#\(title)"
+        
+    }
+}
