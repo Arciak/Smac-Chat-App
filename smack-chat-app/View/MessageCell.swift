caef478abea1c307d463cb69acc0cb3fb324b041
Artur Zarzecki <zarzecki.art@icloud.com>
2021-02-01 15:22:30 +0100

    Display messages on chat window using tableView and Dynamiclly size of the cell (text filed) to the size of the message

---
+++ smack-chat-app/View/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  smack-chat-app
+//
+//  Created by Artur Zarzecki on 01/02/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    //Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+
+}
